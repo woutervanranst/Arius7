31db02c9b1d4ff5eb94ffe19f7c567f744463c60
Wouter Van Ranst <wouter.vanranst@gmail.com>
2026-03-10 12:35:19 +0100

    chore: add core dependencies and test runner setup (task 1.4)

---
+++ tests/Arius.Core.Tests/SmokeTests.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+using TUnit.Core;
+
+namespace Arius.Core.Tests;
+
+public class SmokeTests
+{
+    [Test]
+    public async Task PlaceholderTest()
+    {
+        await Task.CompletedTask;
+    }
+}
