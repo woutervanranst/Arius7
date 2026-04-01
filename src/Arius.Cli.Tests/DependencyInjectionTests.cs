a005f43ec1891c2c390d9b49121ae901a3703df0
Wouter Van Ranst <wouter.vanranst@gmail.com>
2026-04-01 15:56:19 +0200

    test(core): verify streaming ls handler registration

---
+++ src/Arius.Cli.Tests/DependencyInjectionTests.cs
@@ -0,0 +1,36 @@
+using Arius.Core;
+using Arius.Core.Archive;
+using Arius.Core.Ls;
+using Arius.Core.Restore;
+using Arius.Core.Storage;
+using Mediator;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using NSubstitute;
+using Shouldly;
+
+namespace Arius.Cli.Tests;
+
+public class AddAriusRegistrationTests
+{
+    [Test]
+    public void AddArius_RegistersStreamingLsHandlerInterface()
+    {
+        var services = new ServiceCollection();
+        services.AddLogging(b => b.AddProvider(NullLoggerProvider.Instance));
+        services.AddMediator();
+        services.AddArius(
+            blobStorage: Substitute.For<IBlobStorageService>(),
+            passphrase: null,
+            accountName: "test",
+            containerName: "test",
+            cacheBudgetBytes: 0);
+
+        using var serviceProvider = services.BuildServiceProvider();
+
+        serviceProvider.GetRequiredService<IStreamQueryHandler<LsCommand, RepositoryEntry>>().ShouldBeOfType<LsHandler>();
+        serviceProvider.GetRequiredService<ICommandHandler<ArchiveCommand, ArchiveResult>>().ShouldBeOfType<ArchivePipelineHandler>();
+        serviceProvider.GetRequiredService<ICommandHandler<RestoreCommand, RestoreResult>>().ShouldBeOfType<RestorePipelineHandler>();
+    }
+}
