5779b22b0d4c1daee4c25ecc631fdfe9ca6b44db
Wouter Van Ranst <wouter.vanranst@gmail.com>
2026-04-01 12:30:54 +0200

    chore: add Arius.Explorer and Arius.Explorer.Tests verbatim from previous version

---
+++ src/Arius.Explorer/Shared/Extensions/DataProtectionExtensions.cs
@@ -0,0 +1,54 @@
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Arius.Explorer.Shared.Extensions;
+
+public static class DataProtectionExtensions
+{
+    extension(string plainText)
+    {
+        public string Protect()
+        {
+            if (string.IsNullOrEmpty(plainText))
+                return plainText;
+
+            try
+            {
+                var data          = Encoding.UTF8.GetBytes(plainText);
+                var protectedData = ProtectedData.Protect(data, null, DataProtectionScope.CurrentUser);
+                return Convert.ToBase64String(protectedData);
+            }
+            catch (Exception)
+            {
+                // If protection fails, return the original value
+                // This could happen on non-Windows systems or if DPAPI is not available
+                return plainText;
+            }
+        }
+
+        public string Unprotect()
+        {
+            if (string.IsNullOrEmpty(plainText))
+                return plainText;
+
+            try
+            {
+                // First check if it's valid Base64 before attempting to unprotect
+                var protectedData = Convert.FromBase64String(plainText);
+                var data          = ProtectedData.Unprotect(protectedData, null, DataProtectionScope.CurrentUser);
+                return Encoding.UTF8.GetString(data);
+            }
+            catch (FormatException)
+            {
+                // Not valid Base64, likely plain text from before encryption was added
+                return plainText;
+            }
+            catch (Exception)
+            {
+                // If unprotection fails for other reasons, return the original value
+                // This could happen if the data is corrupted or can't be decrypted
+                return plainText;
+            }
+        }
+    }
+}
