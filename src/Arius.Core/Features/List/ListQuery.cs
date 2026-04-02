06719501064ca4fccc696d753fb60af9c174747f
Wouter Van Ranst <wouter.vanranst@gmail.com>
2026-04-02 07:11:01 +0200

    chore: rename

--- src/Arius.Core/Features/List/ListQuery.cs
+++
@@ -1,8 +0,0 @@
-using Mediator;
-
-namespace Arius.Core.Features.List;
-
-/// <summary>
-/// Mediator stream query: list repository entries in a snapshot.
-/// </summary>
-public sealed record ListQuery(ListQueryOptions Options) : IStreamQuery<RepositoryEntry>;
