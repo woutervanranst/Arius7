aee020ec0c0c0751cb039cba5f30aab34e39af28
Wouter Van Ranst <wouter.vanranst@gmail.com>
2026-05-02 07:11:53 +0200

    feat: refactor into field

--- src/Arius.Core/Shared/Hashes/ContentHash.cs
+++ src/Arius.Core/Shared/Hashes/ContentHash.cs
@@ -2,19 +2,17 @@ namespace Arius.Core.Shared.Hashes;
 
 public readonly record struct ContentHash
 {
-    private readonly string _value;
-
     private ContentHash(string value)
     {
         ArgumentException.ThrowIfNullOrEmpty(value);
-        _value = value;
+        Value = value;
     }
 
     public string Prefix4 => Value[..4];
 
     public string Short8 => Value[..8];
 
-    private string Value => _value ?? throw new InvalidOperationException("ContentHash is uninitialized.");
+    private string Value => field ?? throw new InvalidOperationException("ContentHash is uninitialized.");
 
     public static ContentHash Parse(string value) => new(HashCodec.NormalizeHex(value));
 
