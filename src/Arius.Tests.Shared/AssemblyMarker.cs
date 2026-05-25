4eee1bdb1f3c2b74ed1f29377c6d7f182dc5c65b
Wouter Van Ranst <wouter.vanranst@gmail.com>
2026-05-25 09:08:55 +0200

    refactor

--- src/Arius.Tests.Shared/AssemblyMarker.cs
+++ src/Arius.Tests.Shared/AssemblyMarker.cs
@@ -3,6 +3,7 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("Arius.E2E.Tests")]
 [assembly: InternalsVisibleTo("Arius.Core.Tests")]
 [assembly: InternalsVisibleTo("Arius.Integration.Tests")]
+[assembly: InternalsVisibleTo("Arius.Benchmarks")]
 
 namespace Arius.Tests.Shared;
 
